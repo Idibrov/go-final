f3a13093fbfb5937c937669417e384ad8c104cd0
Ivan Dibrov <evrjmet@gmail.com>
2023-06-06 01:03:42 +0300

    final
    Update README.md
    
    fix
    
    add txt
    
    add readme

---
+++ include/server/server.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Idibrov/go-final/include/handler"
+)
+
+func StartServer(addr string) {
+	log.Println("Start server")
+	server := &http.Server{
+		Addr:         addr,
+		Handler:      handler.InitRoutes(),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
